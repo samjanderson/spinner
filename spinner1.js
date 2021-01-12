95c38ff26fdc7f29224a15b120c27265a793f3c5
samjanderson <sam.andersonnn@live.ca>
2021-01-12 16:32:08 +0000

    fixed one small mistake

--- spinner1.js
+++ spinner1.js
@@ -18,7 +18,6 @@ setTimeout(() => {
 }, 700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|   '); 
 }, 900);
 
@@ -36,7 +35,6 @@ setTimeout(() => {
 }, 1500);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write('\r|   '); 
 }, 1700);
 
